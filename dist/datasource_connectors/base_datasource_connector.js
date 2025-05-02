3adbb2e0689ebe160ca901f4d1270c1352b8d1bc
David Matt-Ojo <ojooh@Davids-MacBook-Pro.local>
2025-05-02 23:49:25 +0100

    🔥 Built dbms for publishing

--- dist/datasource_connectors/base_datasource_connector.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseDatasourceConnector {
-    constructor(options) {
-        this.options = options;
-    }
-}
-exports.default = BaseDatasourceConnector;
--- dist/query_builders/base_query_builder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BaseQueryBuilder {
-    // Transaction control
-    beginTransaction() { return 'START TRANSACTION'; }
-    commitTransaction() { return 'COMMIT'; }
-    rollbackTransaction() { return 'ROLLBACK'; }
-}
-exports.default = BaseQueryBuilder;
